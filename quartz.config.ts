c2664663d5aad9b12fa409ed593321a3e0deecf2
Coco1na <motveidv@gmail.com>
2025-04-08 13:21:50 +0600

    vault backup: 2025-04-08 13:21:50

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "База знаний",
+    pageTitle: "БАЗА ЗНАНИЙ",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
